3e2e22c0ccca67c82974a27a2fdcc0bc043fd71a
haido86 <thithuhai.do@gmail.com>
2022-07-15 19:30:40 +0300

    9.1-9.3 final version

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -12,19 +12,19 @@ interface calculateBmiValues {
   weight: number;
  }
 
-const parseArguments = (args: Array<string>): calculateBmiValues => {
-  if (args.length < 4) throw new Error('Not enough arguments');
-  if (args.length > 4) throw new Error('Too many arguments');
+// const parseArguments = (args: Array<string>): calculateBmiValues => {
+//   if (args.length < 4) throw new Error('Not enough arguments');
+//   if (args.length > 4) throw new Error('Too many arguments');
 
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-    return {
-      height: Number(args[2]),
-      weight: Number(args[3]),     
-    }
-  } else {
-    throw new Error('Provided values were not numbers!');
-  }
-}
+//   if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+//     return {
+//       height: Number(args[2]),
+//       weight: Number(args[3]),     
+//     }
+//   } else {
+//     throw new Error('Provided values were not numbers!');
+//   }
+// }
 
 const calculateBmi = (height: number, weight: number) => {
  
--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -1,45 +1,121 @@
-interface Result { periodLength: number,
-    trainingDays: number;
-    success: boolean;
-    rating: number;
-    ratingDescription: string;
-    target: number;
-    average: number ;} 
+// interface Result {
+// periodLength: number,
+//     trainingDays: number;
+//     success: boolean;
+//     rating: number;
+//     ratingDescription: string;
+//     target: number;
+//     average: number ;
+// }
 
-    let sum=0
-    let trainingDays=0
-    let success= true
-    let ratingDescription=''
-    
-   
-const calculateExercises=(input:number[], target:number,rating:number):Result=>{
-   const periodLength= input.length
-    
+//     let sum=0
+//     let trainingDays=0
+//     let success= true
+//     let ratingDescription=''
 
-   for (let i=0; i< input.length; i++){ 
-    sum= sum+input[i]
-    if (input[i]>0){trainingDays=trainingDays+1}}
+// const calculateExercises=(input:number[], target:number,rating:number):Result=>{
+//    const periodLength= input.length
 
-   const average= sum/periodLength
-   console.log("average", average);
-   console.log("target", target); 
-   if (average < target){
-    success = false  
-   }
-   else {
-    success === true
-   }
+//    for (let i=0; i< input.length; i++){
+//     sum= sum+input[i]
+//     if (input[i]>0){trainingDays=trainingDays+1}}
 
-    if (rating===1){ratingDescription='you must try harder'}
-    else if (rating===2){ratingDescription='not too bad but could be better'}
-    else if(rating===3){ratingDescription='you made it'}
-return { periodLength,
+//    const average= sum/periodLength
+
+//    if (average < target){
+//     success = false
+//    }
+//    else {
+//     success === true
+//    }
+//    const rating = average >= 3 ? 3 : average <= 1 ? 1 : 2;
+
+//     if (rating===1){ratingDescription='you must try harder'}
+//     else if (rating===2){ratingDescription='not too bad but could be better'}
+//     else if(rating===3){ratingDescription='you made it'}
+// return { periodLength,
+//     trainingDays,
+//     success,
+//     rating,
+//     ratingDescription,
+//     target,
+//     average}
+// }
+
+// console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1],2,2))
+
+interface calculateExerciseValues {
+  rating: number;
+  target: number;
+  input: [hours: number];
+}
+
+const parseArguments = (args: Array<string>) => {
+  if (args.length <= 1) throw new Error('Not enough arguments');
+
+  const findNaN = args.slice(2).find((item) => isNaN(Number(item)));
+
+  if (!findNaN) {
+    return {
+      target: Number(args[2]),
+      hours: args.slice(3).map((item) => Number(item)),
+    };
+  } else {
+    throw new Error(`Provided values were not numbers!, ${findNaN}`);
+  }
+};
+
+let sum = 0;
+let trainingDays = 0;
+let success = true;
+let ratingDescription = '';
+
+const calculateExercises = (target: number, input: Array<number>) => {
+  const periodLength = input.length;
+
+  for (let i = 0; i < input.length; i++) {
+    sum = sum + input[i];
+    if (input[i] > 0) {
+      trainingDays = trainingDays + 1;
+    }
+  }
+
+  const average = sum / periodLength;
+  console.log('average', average);
+  console.log('target', target);
+  if (average < target) {
+    success = false;
+  } else {
+    success === true;
+  }
+
+  const rating = average >= 3 ? 3 : average <= 1 ? 1 : 2;
+
+  if (rating === 1) {
+    ratingDescription = 'you must try harder';
+  } else if (rating === 2) {
+    ratingDescription = 'not too bad but could be better';
+  } else if (rating === 3) {
+    ratingDescription = 'you made it';
+  }
+  console.log({
+    periodLength,
     trainingDays,
     success,
     rating,
     ratingDescription,
     target,
-    average}
-}
+    average,
+  });
+};
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1],2,2))
+try {
+  const { target, hours } = parseArguments(process.argv);
+  calculateExercises(target, hours);
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
